2bc1b051b476c03305c229d891f163f912d15fa6
Thiago Paranhos Klem <tpklem@gmail.com>
2023-08-02 00:53:00 -0300

    Service layer, component registration

---
+++ src/main/java/com/thiagoklem/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.thiagoklem.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagoklem.course.entities.User;
+import com.thiagoklem.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
