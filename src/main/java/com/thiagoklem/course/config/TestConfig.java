3fa206d7deff4eb71cb7fb05434779ac035aa42b
Thiago Paranhos Klem <tpklem@gmail.com>
2023-08-06 18:52:11 -0300

    Payment, one-to-one association

--- src/main/java/com/thiagoklem/course/config/TestConfig.java
+++ src/main/java/com/thiagoklem/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.thiagoklem.course.entities.Category;
 import com.thiagoklem.course.entities.Order;
 import com.thiagoklem.course.entities.OrderItem;
+import com.thiagoklem.course.entities.Payment;
 import com.thiagoklem.course.entities.Product;
 import com.thiagoklem.course.entities.User;
 import com.thiagoklem.course.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
